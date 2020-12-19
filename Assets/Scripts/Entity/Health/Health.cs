1b649ac6f54bffe38427cf8641236e6f9106fae5
Rhyth <rhythns@gmail.com>
2020-12-19 16:35:27 +0100

    Prepared Files and Scenes for Player infrastructure

---
+++ Assets/Scripts/Entity/Health/Health.cs
@@ -0,0 +1,31 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    [SyncVar] private int max;
+    [SyncVar(hook = nameof(OnCurrentChanged))] private int current;
+
+    public int Max => max;
+    public int Current => current;
+    public int DamageTaken => max - current;
+
+    [Command]
+    public void CmdDamage(int amount)
+    {
+        current = Mathf.Clamp(current - amount, 0, max);
+        if (current == 0)
+            RpcOnDied();
+    }
+
+    [ClientRpc]
+    private void RpcOnDied()
+    {
+        GetComponent<IDieable>().Die();
+    }
+
+    private void OnCurrentChanged(int prevHealth, int currentHealth)
+    {
+        // TODO: Maybe update UI or something
+    }
+}
