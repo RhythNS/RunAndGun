5a462afd07a3ed44ca24381d1e8239b23cc3494c
Rhyth <rhythns@gmail.com>
2021-01-09 19:40:19 +0100

    Added method for getting all gid values from a tiled map

--- Assets/Scripts/MapImporter/TiledImporter.cs
+++ Assets/Scripts/MapImporter/TiledImporter.cs
@@ -36,6 +36,27 @@ public class TiledImporter : MonoBehaviour
     }
 
     /// <summary>
+    /// Loads all objects from all object layers from a map.
+    /// </summary>
+    /// <param name="map">The map where the objects are on.</param>
+    /// <param name="x">The x offset of the map.</param>
+    /// <param name="y">The y offset of the map.</param>
+    /// <param name="loadedGameobjects">A list of all the loaded objects.</param>
+    private void LoadAllObjects(TmxMap map, int x, int y, List<GameObject> loadedGameobjects)
+    {
+        // Go through each objectlayer and spawn their objects
+        for (int i = 0; i < map.ObjectGroups.Count; i++)
+        {
+            for (int j = 0; j < map.ObjectGroups[i].Objects.Count; j++)
+            {
+                if (LoadObject(map, map.ObjectGroups[i].Objects[j], x, y, out GameObject obj))
+                    loadedGameobjects.Add(obj);
+            }
+        }
+    }
+
+
+    /// <summary>
     /// Instantiates a gameobject based on a tiled object.
     /// </summary>
     /// <param name="map">The map where the tiled object is from.</param>
@@ -109,29 +130,10 @@ public class TiledImporter : MonoBehaviour
             for (int t = 0; t < map.TileLayers[l].Tiles.Count; t++)
             {
                 TmxLayerTile layerTile = map.TileLayers[l].Tiles[t];
-                int gid = layerTile.Gid;
 
-                // Empty tile, do nothing
-                if (gid == 0)
+                int gid = GetGid(map, layerTile, out TmxTileset toFindTileset);
+                if (gid == -1)
                     continue;
-
-                // Get the acctual gid value
-                gid--;
-
-                // Get the correct tileset with the gid value
-                TmxTileset toFindTileset = null;
-                for (int j = 0; j < map.Tilesets.Count; j++)
-                {
-                    gid -= map.Tilesets[j].TileCount ?? 0;
-                    if (gid <= 0)
-                    {
-                        toFindTileset = map.Tilesets[j];
-                        break;
-                    }
-                }
-
-                // set gid to a positive value again
-                gid += toFindTileset.TileCount ?? 0;
 
                 Texture2D tilesetImage = toFindTileset.Image;
 
@@ -144,6 +146,7 @@ public class TiledImporter : MonoBehaviour
                 int row = gid / columns;
 
                 // If the tileset tile has objects, spawn them
+                /*
                 if (toFindTileset.Tiles.TryGetValue(gid, out TmxTilesetTile tilesetTile) == true)
                 {
                     for (int groupIndex = 0; groupIndex < tilesetTile.ObjectGroups.Count; groupIndex++)
@@ -155,6 +158,7 @@ public class TiledImporter : MonoBehaviour
                         }
                     }
                 }
+                 */
 
                 // Create and set the tile on the tilemap
                 Tile tile = ScriptableObject.CreateInstance<Tile>();
@@ -169,20 +173,85 @@ public class TiledImporter : MonoBehaviour
                     tileHeight
                     );
 
-                tileMap.SetTile(new Vector3Int(layerTile.X + x, map.Height - layerTile.Y + y, l), tile);
+                tileMap.SetTile(new Vector3Int(layerTile.X + x, map.Height - layerTile.Y - 1 + y, l), tile);
             }
         }
 
-        // Go through each objectlayer and spawn their objects
-        for (int i = 0; i < map.ObjectGroups.Count; i++)
+        // Load objects from the maps objectgroups
+        LoadAllObjects(map, x, y, loadedGameobjects);
+    }
+
+    /// <summary>
+    /// Gets the gid values of the tiles in an array. The gid are not the acctual gid values of the map, but the gid
+    /// values of the tileset. Assume tileset a has 10 tiles and tileset b has 10 tiles. If at position 1 there is
+    /// the tileset tile 1 of tileset a and at position 2 the tileset tile 1 of tileset b then the gid values of the
+    /// return array are the same. To put it simply, just use one tileset and one layer for this method.
+    /// </summary>
+    /// <param name="mapName">The name of the map to get the gid values from.</param>
+    /// <param name="atX">The x position of where the map in the Tilemap will be placed.</param>
+    /// <param name="atY">The y position of where the map in the Tilemap will be placed.</param>
+    /// <param name="loadedGameobjects">A list of all loaded GameObjects.</param>
+    /// <returns>The gid values of the map.</returns>
+    public Fast2DArray<int> GetReplacableMap(string mapName, int atX, int atY, out List<GameObject> loadedGameobjects)
+    {
+        loadedGameobjects = new List<GameObject>();
+
+        TmxMap map = LoadMap(mapName);
+        Fast2DArray<int> retArr = new Fast2DArray<int>(map.Width, map.Height);
+
+        if (map.TileLayers.Count == 0)
+            throw new Exception("Replacable map " + mapName + " has no tile layers!");
+        else if (map.TileLayers.Count > 1)
+            Debug.LogWarning("Replacable map " + mapName + " has more than 1 tile layer. I am only reading the first layer!");
+        if (map.Tilesets.Count != 1)
+            Debug.LogWarning("Replacable map " + mapName + " has not only 1 tileset. This might cause issues!");
+
+        for (int t = 0; t < map.TileLayers[0].Tiles.Count; t++)
         {
-            for (int j = 0; j < map.ObjectGroups[i].Objects.Count; j++)
+            TmxLayerTile layerTile = map.TileLayers[0].Tiles[t];
+            int gid = GetGid(map, layerTile, out _);
+            retArr.Set(gid, layerTile.X, map.Height - layerTile.Y - 1);
+        }
+
+        LoadAllObjects(map, atX, atY, loadedGameobjects);
+
+        return retArr;
+    }
+
+    /// <summary>
+    /// Gets the acctual gid of a TilesetTile and returns the tileset that the tile is from-
+    /// </summary>
+    /// <param name="map">The map where the TilesetTile is from.</param>
+    /// <param name="layerTile">The TilesetTile to find the correct gid from.</param>
+    /// <param name="toFindTileset">The correct Tileset.</param>
+    /// <returns>The gid value.</returns>
+    private int GetGid(TmxMap map, TmxLayerTile layerTile, out TmxTileset toFindTileset)
+    {
+        int gid = layerTile.Gid;
+        toFindTileset = null;
+
+        // Empty tile, do nothing
+        if (gid == 0)
+            return -1;
+
+        // Get the acctual gid value
+        gid--;
+
+        // Get the correct tileset with the gid value
+        for (int j = 0; j < map.Tilesets.Count; j++)
+        {
+            gid -= map.Tilesets[j].TileCount ?? 0;
+            if (gid <= 0)
             {
-                if (LoadObject(map, map.ObjectGroups[i].Objects[j], x, y, out GameObject obj))
-                    loadedGameobjects.Add(obj);
+                toFindTileset = map.Tilesets[j];
+                break;
             }
         }
+
+        // set gid to a positive value again
+        return gid + toFindTileset.TileCount ?? 0;
     }
+
 
     private void OnDestroy()
     {
