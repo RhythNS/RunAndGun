87febf481228d985415f87df7d5dc7d85d96cf97
Rhyth <rhythns@gmail.com>
2021-01-13 17:38:26 +0100

    Started Mobile Input

---
+++ Assets/Scripts/Entity/Player/Input/Mobile/MobileInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : RAGInput
+{
+    public override InputType InputType => InputType.Mobile;
+
+    public override void Remove()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool GetDashInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool GetFireInput(out Vector2 fireDirection)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override Vector2 GetMovementInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool GetReloadInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Pickup()
+    {
+        throw new System.NotImplementedException();
+    }
+}
