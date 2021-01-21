9e41050f42ac9e019f68b188f1f4938e41743c97
Kiminaze <kiminaze@yahoo.de>
2021-01-21 02:50:50 +0100

    More UI stuff

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("LocalPlayer")]
+    [SerializeField]
+    private Text textHpNum;
+    [SerializeField]
+    private Image imgHpBar;
+    [SerializeField]
+    private Image imgPlayerIcon;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Other Players")]
+    [SerializeField]
+    private Text[] textPlayersHpNum;
+    [SerializeField]
+    private Image[] imgPlayersHpBar;
+    [SerializeField]
+    private Image[] imgPlayersPlayerIcon;
+
+    [Header("Boss")]
+    [SerializeField]
+    private Text textBossHpNum;
+    [SerializeField]
+    private Image imgBossHpBar;
+    [SerializeField]
+    private Image imgBossIcon;
+
+    [Header("Weapons")]
+    [SerializeField]
+    private Text textWeaponName;
+    [SerializeField]
+    private Text textAmmoCount;
+    [SerializeField]
+    private Image imgWeaponIcon;
 }
