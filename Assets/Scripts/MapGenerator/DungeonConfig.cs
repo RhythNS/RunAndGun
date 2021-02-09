9d22417a0049823f6c99a9fae92011896cdebda0
Kiminaze <kiminaze@yahoo.de>
2021-02-09 02:02:36 +0100

    Started Mapconfig

---
+++ Assets/Scripts/MapGenerator/DungeonConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MapGenerator
+{
+    /// <summary>
+    /// Config used to initialize a Dungeon.
+    /// </summary>
+    public struct DungeonConfig
+    {
+        public Vector2Int maxSize;
+
+        public int seed;
+
+        public int minRooms;
+        public int maxRooms;
+
+        public int corridorMinLength;
+        public int corridorMaxLength;
+
+
+    }
+}
