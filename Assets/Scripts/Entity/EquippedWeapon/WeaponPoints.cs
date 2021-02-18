de5ebd1aad087bb8e6174ebb14e8d29fc1d5020c
Rhyth <rhythns@gmail.com>
2021-02-18 16:57:09 +0100

    Added two handed weapon animators

--- Assets/Scripts/Entity/EquippedWeapon/WeaponPoints.cs
+++ Assets/Scripts/Entity/EquippedWeapon/WeaponPoints.cs
@@ -14,19 +14,27 @@ public class WeaponPoints : MonoBehaviour
     public Vector2 SouthPoint => southPoint;
     [SerializeField] private Vector2 southPoint;
 
+    public Vector2 TwoHandedMidPoint => twoHandedMidPoint;
+    [SerializeField] private Vector2 twoHandedMidPoint;
+
+    public float Radius => radius;
+    [SerializeField] private float radius;
+
     private void OnDrawGizmosSelected()
     {
+        Gizmos.color = Color.red;
         DrawGizmoPoint(eastHand);
         DrawGizmoPoint(westHand);
         DrawGizmoPoint(northPoint);
         DrawGizmoPoint(southPoint);
+
+        Gizmos.DrawWireSphere(transform.position + (Vector3)twoHandedMidPoint, radius);
     }
 
     private void DrawGizmoPoint(Vector2 point)
     {
         Vector3 position = point;
         position += transform.position;
-        Gizmos.color = Color.red;
         Gizmos.DrawLine(position + new Vector3(-0.125f, -0.125f), position + new Vector3(0.125f, 0.125f));
         Gizmos.DrawLine(position + new Vector3(-0.125f, 0.125f), position + new Vector3(0.125f, -0.125f));
     }
