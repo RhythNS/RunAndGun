ff8531dc7a6935d229fce240d24206239f5f637a
Rhyth <rhythns@gmail.com>
2021-08-02 15:01:11 +0200

    Fixed: Players could not shoot directly up and stopped Bullets hitting Pickables

---
+++ Assets/Scripts/DebugAndTesting/PrintHitUIElement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PrintHitUIElement : MonoBehaviour
+{
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            foreach (RaycastResult result in raycastResults)
+            {
+
+                Debug.Log("Mouse hit: " + result.gameObject.name);
+            }
+        }
+    }
+}
