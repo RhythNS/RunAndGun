221eb66df4e3010c7c50a1af596d011885550b52
Kiminaze <kiminaze@yahoo.de>
2021-08-23 04:14:13 +0200

    New BT Nodes, Two new enemies

---
+++ Assets/Scripts/Entity/Enemy/BehaviourTree/Bools/IsHealthAboveValue.cs
@@ -0,0 +1,27 @@
+﻿using Rhyth.BTree;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Returns true if the given entity is still alive.
+/// </summary>
+public class IsHealthAboveValue : BoolNode
+{
+    public override string StringToolTip => "Returns true if own health is above percentage.";
+
+    [SerializeField] private float percentage;
+
+    protected override BNode InnerClone(Dictionary<Value, Value> originalValueForClonedValue)
+    {
+        IsHealthAboveValue ihav = CreateInstance<IsHealthAboveValue>();
+        ihav.percentage = percentage;
+        return ihav;
+    }
+
+    protected override bool InnerIsFulfilled()
+    {
+        Health health = tree.AttachedBrain.GetComponent<Health>();
+        
+        return health.Current / health.Max > percentage;
+    }
+}
---
+++ Assets/Scripts/Entity/Enemy/BehaviourTree/Bools/IsInWeaponRange.cs
@@ -0,0 +1,32 @@
+﻿using Rhyth.BTree;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Returns true if the given entity is still alive.
+/// </summary>
+public class IsInWeaponRange : BoolNode
+{
+    public override string StringToolTip => "Returns true if target is in range of current weapon.";
+
+    [SerializeField] private HealthValue targetHealth;
+
+    protected override BNode InnerClone(Dictionary<Value, Value> originalValueForClonedValue)
+    {
+        IsInWeaponRange iiwr = CreateInstance<IsInWeaponRange>();
+        iiwr.targetHealth = CloneValue(originalValueForClonedValue, targetHealth) as HealthValue;
+        return iiwr;
+    }
+
+    protected override bool InnerIsFulfilled()
+    {
+        float range = tree.AttachedBrain.GetComponent<EquippedWeapon>().Weapon.Range;
+
+        Vector2 targetPosition = targetHealth.Get().transform.position;
+        Vector2 ownPosition = Brain.transform.position;
+
+        Vector2 direction = ownPosition - targetPosition;
+
+        return direction.magnitude < range;
+    }
+}
